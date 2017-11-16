dc21e6e844c243ec381d0b40033b990f123c8f99
maloki <malokiusa@gmail.com>
2017-11-16 12:10:25 +0100

    setting up heroku

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Routes } from './routes.js'
-import reducers from './reducers';
-//const store = createStoreWithMiddleware(reducers, mergedState);
-
-ReactDOM.render(<Routes />,
-  document.getElementById('root'))
