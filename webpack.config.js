2dde60a95b79ad724f633b9013a4fa3757d2e1c7
maloki <malokiusa@gmail.com>
2017-11-16 12:16:24 +0100

    setting up heroku

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['es2015', 'stage-1', 'react']
+                    presets: ['es2015', 'stage-2', 'react']
                 }
             },
             {
