6797d8e5662d8955812e7eca470d57c75dd21fd7
maloki <malokiusa@gmail.com>
2017-11-16 11:57:59 +0100

    setting up heroku

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.get('*', (req, res, url) => {
           const RenderedApp = renderFullPage(ReactApp)
           res.send(RenderedApp)
       })
-})
+}) 
 
 function renderFullPage(html) {
   return `
@@ -32,7 +32,7 @@ function renderFullPage(html) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link rel="stylesheet" href="/styles/styles.css" />
-      </head> 
+      </head>
       <body>
         <style>
         </style>
